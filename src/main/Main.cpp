2b73ba3f1bb018eb03d6458ec1e05531af986ece
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-06-22 20:58:22 +0200

    sample main

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <string>
+// #include "./models/Account.h"
+#include "./services/UserService.h"
 using namespace std;
+
 
 int main( ){
 
     cout << "Development of Bank begins Here." << endl;
+    User newUser;
+    newUser.setFirstName("SAUVE");
+    newUser.setLastName("Jean-Luc");
+    newUser.setEmail("me@gmail.com");
+    newUser.setPassword("12345");
+
+    Account account;
+    account.setId(1);
+    account.setUser(newUser);
+
+
+    // cout << newUser.getLastName() << endl;
+    // cout << account.getUser().getLastName() << endl;
+
+    cout << string::npos << endl;
+
+    UserService userService;
+
+    userService.registerUser(newUser);
+    string email = "me@gmail.com";
+    string password = "12345";
+
+    User testUser;
+    testUser = userService.login(email,password);
+
+    cout << "Test user username: " <<  testUser.getFirstName();
+    
+
 
 }
