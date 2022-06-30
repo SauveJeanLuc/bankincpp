1a5e8d7c858fea7a667e67b7588abaad88fe691b
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-06-30 17:10:40 +0200

    refine Navigator

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -1,50 +1,15 @@
 #include <iostream>
 #include <string>
-#include "./services/UserService.h"
+#include "./controllers/Navigator.h"
 using namespace std;
 
 
 int main( ){
+    Navigator navigator;
 
-    // cout << "Development of Bank begins Here." << endl;
-    // User newUser;
-    // newUser.setFirstName("SAUVE");
-    // newUser.setLastName("Jean-Luc");
-    // newUser.setEmail("me@gmail.com");
-    // newUser.setPassword("12345");
-
-    // Account account;
-    // account.setId(1);
-    // account.setUser(newUser);
-
-
-    // // cout << newUser.getLastName() << endl;
-    // // cout << account.getUser().getLastName() << endl;
-
-    // cout << string::npos << endl;
-
-    // UserService userService;
-
-    // userService.registerUser(newUser);
-    // string email = "me@gmail.com";
-    // string password = "12345";
-
-    // User testUser;
-    // testUser = userService.login(email,password);
-
-    // cout << "Test user username: " <<  testUser.getFirstName();
-    
-    AccountService accountService;
-    // Account fetchedAccount = accountService.getAccountById(1);
-
-    // UserService userService;
-    // User user = userService.getUserById(1);
-    
-    // cout << " Fetched account's user firstName: " << fetchedAccount.getUser().getFirstName() << endl;
-    // cout << " Fetched user's user firstName: " << user.getFirstName() << endl;
-    // accountService.depositOrWithdraw(1, 100.00, true);
-    accountService.depositOrWithdraw(1, 100.00, false);
-    // accountService.tryUpdatingFile();
+    while(1){
+        navigator.start();
+    }
 
     return 1;
 }
